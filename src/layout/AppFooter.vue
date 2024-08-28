b2ee1b547daa00b4423644ff1d7a80d94ad79ef3
Astesia <2689324970@qq.com>
2024-08-28 11:11:29 +0800

    edit footer

--- src/layout/AppFooter.vue
+++ src/layout/AppFooter.vue
@@ -2,7 +2,8 @@
 
 <template>
     <div class="layout-footer">
-        SAKAI by
+        Vehiclism by
         <a href="https://primevue.org" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline">PrimeVue</a>
+        暑期实训第六小组
     </div>
 </template>
