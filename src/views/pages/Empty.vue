778542c8e8c6e7845dd613f30706f8d4bc4c0c0e
Astesia <2689324970@qq.com>
2024-08-28 08:56:17 +0800

    Your commit message

--- src/views/pages/Empty.vue
+++ src/views/pages/Empty.vue
@@ -1,6 +1,6 @@
 <template>
     <div className="card">
         <div class="font-semibold text-xl mb-4">Empty Page</div>
-        <p>Use this page to start from scratch and place your custom content.</p>
+        <p>git push -u origin master</p>
     </div>
 </template>
