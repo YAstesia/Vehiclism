086709057fca4c4386433b0a37ecd754304067f9
Astesia <2689324970@qq.com>
2024-08-30 15:59:28 +0800

    edit dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -42,7 +42,7 @@ function setChartData() {
             },
             {
                 type: 'bar',
-                label: '纯电动汽车',
+                label: '新能源汽车',
                 backgroundColor: documentStyle.getPropertyValue('--p-primary-200'),
                 data: [4100, 5200, 6400, 7400],
                 borderRadius: {
@@ -107,18 +107,18 @@ watch([getPrimary, getSurface, isDarkTheme], () => {
             <div class="card">
                 <div class="font-semibold text-xl mb-4">近期汽车销量</div>
                 <DataTable :value="products" :rows="5" :paginator="true" responsiveLayout="scroll">
-                    <Column style="width: 15%" header="Image">
+                    <Column style="width: 15%" header="图片">
                         <template #body="slotProps">
                             <img :src="`https://primefaces.org/cdn/primevue/images/product/${slotProps.data.image}`" :alt="slotProps.data.image" width="50" class="shadow" />
                         </template>
                     </Column>
-                    <Column field="name" header="Name" :sortable="true" style="width: 35%"></Column>
-                    <Column field="price" header="Price" :sortable="true" style="width: 35%">
+                    <Column field="name" header="车型" :sortable="true" style="width: 35%"></Column>
+                    <Column field="price" header="价格" :sortable="true" style="width: 35%">
                         <template #body="slotProps">
                             {{ formatCurrency(slotProps.data.price) }}
                         </template>
                     </Column>
-                    <Column style="width: 15%" header="View">
+                    <Column style="width: 15%" header="查看详情">
                         <template #body>
                             <Button icon="pi pi-search" type="button" class="p-button-text"></Button>
                         </template>
@@ -127,7 +127,7 @@ watch([getPrimary, getSurface, isDarkTheme], () => {
             </div>
             <div class="card">
                 <div class="flex justify-between items-center mb-6">
-                    <div class="font-semibold text-xl">销量热点趋势</div>
+                    <div class="font-semibold text-xl">7日内热销品牌 Top3</div>
                 </div>
                 <ul class="list-none p-0 m-0">
                     <li class="flex flex-col md:flex-row md:items-center md:justify-between mb-6">
@@ -137,9 +137,9 @@ watch([getPrimary, getSurface, isDarkTheme], () => {
                         </div>
                         <div class="mt-2 md:mt-0 flex items-center">
                             <div class="bg-surface-300 dark:bg-surface-500 rounded-border overflow-hidden w-40 lg:w-24" style="height: 8px">
-                                <div class="bg-orange-500 h-full" style="width: 50%"></div>
+                                <div class="bg-orange-500 h-full" style="width: 35%"></div>
                             </div>
-                            <span class="text-orange-500 ml-4 font-medium">50%</span>
+                            <span class="text-orange-500 ml-4 font-medium">35%</span>
                         </div>
                     </li>
                     <li class="flex flex-col md:flex-row md:items-center md:justify-between mb-6">
@@ -149,9 +149,9 @@ watch([getPrimary, getSurface, isDarkTheme], () => {
                         </div>
                         <div class="mt-2 md:mt-0 ml-0 md:ml-20 flex items-center">
                             <div class="bg-surface-300 dark:bg-surface-500 rounded-border overflow-hidden w-40 lg:w-24" style="height: 8px">
-                                <div class="bg-cyan-500 h-full" style="width: 16%"></div>
+                                <div class="bg-cyan-500 h-full" style="width: 22%"></div>
                             </div>
-                            <span class="text-cyan-500 ml-4 font-medium">16%</span>
+                            <span class="text-cyan-500 ml-4 font-medium">22%</span>
                         </div>
                     </li>
                     <li class="flex flex-col md:flex-row md:items-center md:justify-between mb-6">
@@ -161,45 +161,9 @@ watch([getPrimary, getSurface, isDarkTheme], () => {
                         </div>
                         <div class="mt-2 md:mt-0 ml-0 md:ml-20 flex items-center">
                             <div class="bg-surface-300 dark:bg-surface-500 rounded-border overflow-hidden w-40 lg:w-24" style="height: 8px">
-                                <div class="bg-pink-500 h-full" style="width: 67%"></div>
+                                <div class="bg-pink-500 h-full" style="width: 14%"></div>
                             </div>
-                            <span class="text-pink-500 ml-4 font-medium">67%</span>
-                        </div>
-                    </li>
-                    <li class="flex flex-col md:flex-row md:items-center md:justify-between mb-6">
-                        <div>
-                            <span class="text-surface-900 dark:text-surface-0 font-medium mr-2 mb-1 md:mb-0">Wonders Notebook</span>
-                            <div class="mt-1 text-muted-color">Office</div>
-                        </div>
-                        <div class="mt-2 md:mt-0 ml-0 md:ml-20 flex items-center">
-                            <div class="bg-surface-300 dark:bg-surface-500 rounded-border overflow-hidden w-40 lg:w-24" style="height: 8px">
-                                <div class="bg-green-500 h-full" style="width: 35%"></div>
-                            </div>
-                            <span class="text-primary ml-4 font-medium">35%</span>
-                        </div>
-                    </li>
-                    <li class="flex flex-col md:flex-row md:items-center md:justify-between mb-6">
-                        <div>
-                            <span class="text-surface-900 dark:text-surface-0 font-medium mr-2 mb-1 md:mb-0">Mat Black Case</span>
-                            <div class="mt-1 text-muted-color">Accessories</div>
-                        </div>
-                        <div class="mt-2 md:mt-0 ml-0 md:ml-20 flex items-center">
-                            <div class="bg-surface-300 dark:bg-surface-500 rounded-border overflow-hidden w-40 lg:w-24" style="height: 8px">
-                                <div class="bg-purple-500 h-full" style="width: 75%"></div>
-                            </div>
-                            <span class="text-purple-500 ml-4 font-medium">75%</span>
-                        </div>
-                    </li>
-                    <li class="flex flex-col md:flex-row md:items-center md:justify-between mb-6">
-                        <div>
-                            <span class="text-surface-900 dark:text-surface-0 font-medium mr-2 mb-1 md:mb-0">Robots T-Shirt</span>
-                            <div class="mt-1 text-muted-color">Clothing</div>
-                        </div>
-                        <div class="mt-2 md:mt-0 ml-0 md:ml-20 flex items-center">
-                            <div class="bg-surface-300 dark:bg-surface-500 rounded-border overflow-hidden w-40 lg:w-24" style="height: 8px">
-                                <div class="bg-teal-500 h-full" style="width: 40%"></div>
-                            </div>
-                            <span class="text-teal-500 ml-4 font-medium">40%</span>
+                            <span class="text-pink-500 ml-4 font-medium">14%</span>
                         </div>
                     </li>
                 </ul>
@@ -207,69 +171,63 @@ watch([getPrimary, getSurface, isDarkTheme], () => {
         </div>
         <div class="col-span-12 xl:col-span-6">
             <div class="card">
-                <div class="font-semibold text-xl mb-4">每日销售行情</div>
+                <div class="font-semibold text-xl mb-4">每日销售行情（单位：辆）</div>
                 <Chart type="bar" :data="chartData" :options="chartOptions" class="h-80" />
             </div>
             <div class="card">
                 <div class="flex items-center justify-between mb-6">
-                    <div class="font-semibold text-xl">最新通知</div>
+                    <div class="font-semibold text-xl">最新汽车销量数据</div>
                 </div>
 
-                <span class="block text-muted-color font-medium mb-4">今天</span>
-                <ul class="p-0 mx-0 mt-0 mb-6 list-none">
-                    <li class="flex items-center py-2 border-b border-surface">
-                        <div class="w-12 h-12 flex items-center justify-center bg-blue-100 dark:bg-blue-400/10 rounded-full mr-4 shrink-0">
-                            <i class="pi pi-dollar !text-xl text-blue-500"></i>
-                        </div>
-                        <span class="text-surface-900 dark:text-surface-0 leading-normal"
-                            >我
-                            <span class="text-surface-700 dark:text-surface-100">去梅园食堂吃午饭花了 <span class="text-primary font-bold">$19.00</span></span>
-                        </span>
-                    </li>
-                    <li class="flex items-center py-2">
-                        <div class="w-12 h-12 flex items-center justify-center bg-orange-100 dark:bg-orange-400/10 rounded-full mr-4 shrink-0">
-                            <i class="pi pi-download !text-xl text-orange-500"></i>
-                        </div>
-                        <span class="text-surface-700 dark:text-surface-100 leading-normal">我花了 <span class="text-primary font-bold">$6380.00</span> 交学费</span>
-                    </li>
-                </ul>
-
-                <span class="block text-muted-color font-medium mb-4">昨天</span>
                 <ul class="p-0 m-0 list-none mb-6">
                     <li class="flex items-center py-2 border-b border-surface">
                         <div class="w-12 h-12 flex items-center justify-center bg-blue-100 dark:bg-blue-400/10 rounded-full mr-4 shrink-0">
                             <i class="pi pi-dollar !text-xl text-blue-500"></i>
                         </div>
                         <span class="text-surface-900 dark:text-surface-0 leading-normal"
-                            >我
-                            <span class="text-surface-700 dark:text-surface-100">去梅园食堂吃午饭花了 <span class="text-primary font-bold">$17.00</span></span>
+                            >
+                            <span class="text-surface-700 dark:text-surface-100 font-bold">2024年6月汽车销量 <span class="text-primary font-bold" style="font-size: 22pt; margin-left: 5px; margin-right: 5px;">177.1</span>万辆</span>
                         </span>
                     </li>
+                </ul>
+
+                <ul class="p-0 m-0 list-none mb-6">
                     <li class="flex items-center py-2 border-b border-surface">
                         <div class="w-12 h-12 flex items-center justify-center bg-pink-100 dark:bg-pink-400/10 rounded-full mr-4 shrink-0">
-                            <i class="pi pi-question !text-xl text-pink-500"></i>
+                            <i class="pi pi-dollar !text-xl text-pink-500"></i>
                         </div>
                         <span class="text-surface-900 dark:text-surface-0 leading-normal"
-                            >我
-                            <span class="text-surface-700 dark:text-surface-100">买了梅园面包店的小面包当下午茶</span>
+                            >
+                            <span class="text-surface-700 dark:text-surface-100 font-bold">2024年6月新能源汽车销量<span class="text-primary font-bold" style="font-size: 22pt; margin-left: 5px; margin-right: 5px;">83.1</span>万辆</span>
                         </span>
                     </li>
                 </ul>
-                <span class="block text-muted-color font-medium mb-4">之前</span>
-                <ul class="p-0 m-0 list-none">
+
+                <ul class="p-0 m-0 list-none mb-6">
                     <li class="flex items-center py-2 border-b border-surface">
-                        <div class="w-12 h-12 flex items-center justify-center bg-green-100 dark:bg-green-400/10 rounded-full mr-4 shrink-0">
-                            <i class="pi pi-arrow-up !text-xl text-green-500"></i>
+                        <div class="w-12 h-12 flex items-center justify-center bg-red-100 dark:bg-blue-400/10 rounded-full mr-4 shrink-0">
+                            <i class="pi pi-dollar !text-xl text-red-500"></i>
                         </div>
-                        <span class="text-surface-900 dark:text-surface-0 leading-normal">我的伙食费开销增加了 <span class="text-primary font-bold">25%</span>.</span>
-                    </li>
-                    <li class="flex items-center py-2 border-b border-surface">
-                        <div class="w-12 h-12 flex items-center justify-center bg-purple-100 dark:bg-purple-400/10 rounded-full mr-4 shrink-0">
-                            <i class="pi pi-heart !text-xl text-purple-500"></i>
-                        </div>
-                        <span class="text-surface-900 dark:text-surface-0 leading-normal"><span class="text-primary font-bold">114514</span> 个大一新生和我在食堂抢饭吃</span>
+                        <span class="text-surface-900 dark:text-surface-0 leading-normal"
+                            >
+                            <span class="text-surface-700 dark:text-surface-100 font-bold">2024年汽车累计销量 <span class="text-primary font-bold" style="font-size: 22pt; margin-left: 5px; margin-right: 5px;">992.5</span>万辆</span>
+                        </span>
                     </li>
                 </ul>
+
+
+                <ul class="p-0 m-0 list-none mb-6">
+                    <li class="flex items-center py-2 border-b border-surface">
+                        <div class="w-12 h-12 flex items-center justify-center bg-green-100 dark:bg-blue-400/10 rounded-full mr-4 shrink-0">
+                            <i class="pi pi-dollar !text-xl text-green-500"></i>
+                        </div>
+                        <span class="text-surface-900 dark:text-surface-0 leading-normal"
+                            >
+                            <span class="text-surface-700 dark:text-surface-100 font-bold">2024年新能源汽车累计销量 <span class="text-primary font-bold" style="font-size: 22pt; margin-left: 5px; margin-right: 5px;">405.2</span>万辆</span>
+                        </span>
+                    </li>
+                </ul>
+
             </div>
         </div>
     </div>
